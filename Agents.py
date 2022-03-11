4f3bb3310bc08f6ca076028d729c315f2ba0846a
Rayaq Siddiqui <rayaq05@hotmail.com>
2022-03-11 12:37:26 -0500

    first commit

---
+++ Agents.py
@@ -0,0 +1,186 @@
+# this is going to be the Agent that interacts with the environment given the state
+# there should be 3 main functionalities
+#     1. act (Agent decides the action based on the state)
+#     2. observe (Agent captures the state and the target Q values)
+#     3. replay (Agent trains the model based on the observation)
+
+# libraries
+import math
+from Memory import Memory
+from DQN import DQN
+import numpy as np
+import random
+from helper_functions import sel_action, sel_action_index
+
+
+# Agent and Random Agent Implementations
+# defining global variables
+
+max_reward = 10
+grass_penalty = 0.4
+action_repeat_num = 8
+max_num_episodes = 1000
+memory_size = 10000
+max_num_steps = action_repeat_num * 100
+gamma = 0.99
+max_eps = 0.1
+min_eps = 0.02
+EXPLORATION_STOP = int(max_num_steps * 10)
+_lambda_ = - np.log(0.001) / EXPLORATION_STOP
+UPDATE_TARGET_FREQUENCY = int(50)
+batch_size = 128
+
+
+# creating the Agent itself
+
+class Agent:
+
+    # constructor
+    def __init__(self, num_states, num_actions, img_dim, model_path):
+
+        # print field
+        print('num_states:', num_states)
+        print('num_actions:', num_actions)
+        print('img_dim:', img_dim)
+        print('model_path:', model_path)
+
+        # global datafield
+        self.steps = 0
+        self.epsilon = max_eps
+        self.memory = Memory(memory_size)
+
+        # const passed in datafield
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.DQN = DQN(num_states, num_actions, model_path)
+        self.no_state = np.zeros(num_states)
+        self.x = np.zeros((batch_size,)+img_dim)
+        self.y = np.zeros([batch_size, num_actions])
+        self.errors = np.zeros(batch_size)
+        self.rand = False
+
+        self.agent_type = 'Learning'
+        self.maxEpsilon = max_eps
+
+
+    # act function (#1)
+    def act(self, s):
+
+        print(self.epsilon)
+
+        if random.random() < self.epsilon:
+            best_act = np.random.randint(self.num_actions)
+            self.rand = True
+            return sel_action(best_act), sel_action(best_act)
+        else:
+            act_soft = self.DQN.predict_single_state(s)
+            best_act = np.argmax(act_soft)
+            self.rand = False
+            return sel_action(best_act), act_soft
+
+
+    # compute targets function used by the observe function (#2) and replay function (#3)
+    def compute_targets(self, batch):
+
+        '''
+            0 -> Index for current state
+            1 -> Index for action
+            2 -> Index for reward
+            3 -> Index for next state
+        '''
+        states = np.array([ rec[1][0] for rec in batch ])
+        states_ = np.array([ (self.no_state if rec[1][3] is None else rec[1][3]) for rec in batch ])
+
+        p = self.DQN.predict(states)
+        p_ = self.DQN.predict(states_, target=False)
+        p_t = self.DQN.predict(states_, target=True)
+
+        # action counter
+        act_ctr = np.zeros(self.num_actions)
+
+        for i in range(len(batch)):
+            rec = batch[i][1]
+            s, a, r, s_ = rec[0], rec[1], rec[2], rec[3]
+
+            a = sel_action_index(a)
+            t = p[i]
+            act_ctr[a] += 1
+
+            oldVal = t[a]
+            if s_ is None:
+                t[a] = r
+            else:
+                # DDQN
+                t[a] = r + gamma * p_t[i][ np.argmax(p_[i]) ]
+
+            self.x[i] = s
+            self.y[i] = t
+
+            if self.steps % 20 ==0 and i == len(batch)-1:
+                print('t',t[a], 'r: %.4f' % r,'mean t',np.mean(t))
+                print ('act ctr: ', act_ctr)
+
+            self.errors[i] = abs(oldVal - t[a])
+
+        return (self.x, self.y, self.errors)
+
+
+    # observe function (#2)
+    # in (s, a, r, s_) format
+    def observe(self, sample):
+        _, _, errors = self.compute_targets([(0, sample)])
+        self.memory.add(errors[0], sample)
+
+        if self.steps % UPDATE_TARGET_FREQUENCY == 0:
+            self.DQN.target_model_update()
+        self.steps += 1
+        self.epsilon = min_eps + (self.maxEpsilone - min_eps) * np.exp(-1*_lambda_ * self.steps)
+
+
+    # replay function (#3)
+    def replay(self):
+        batch = self.memory.sample(batch_size)
+        x, y, errors = self.compute_targets(batch)
+        for i in range(len(batch)):
+            idx = batch[i][0]
+            self.memory.update(idx, errors[i])
+
+        self.DQN.train(x, y)
+
+
+# Random Agent class defined similar to the agent class
+
+class RandomAgent:
+
+    # constructor
+    def __init__(self, num_actions):
+
+        # global datafield
+        self.memory = Memory(memory_size)
+        self.exp = 0
+        self.steps = 0
+
+        # const passed in datafield
+        self.num_actions = num_actions
+        self.agent_type = 'Learning'
+        self.rand = True
+
+
+    # act function (#1)
+    def act(self, s):
+        best_act = np.random.randint(self.num_actions)
+        return sel_action(best_act), sel_action(best_act)
+
+
+    # observe function (#2)
+    # in (s, a, r, s_) format
+    def observe(self, sample):
+        error = abs(sample[2])  # reward
+        self.memory.add(error, sample)
+        self.exp += 1
+        self.steps += 1
+
+
+    # replay function (#3)
+    def replay(self):
+        pass
---
+++ DQN.py
@@ -0,0 +1,134 @@
+# this is going to be the DQN itself
+# understand that this will be using concepts regarding double DQN
+# essentially, we are training one model and copying over the weights to the other model
+# libraries to be imported
+from gc import callbacks
+from matplotlib.pyplot import plot
+import tensorflow as tf
+from tensorflow.keras import optimizers, losses
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Concatenate, Dropout, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import ModelCheckpoint
+import numpy as np
+
+
+# GLOBAL VARIABLES
+learning_rate = 0.0001
+BATCH_SIZE = 128
+
+
+# the class for the DQN
+class DQN:
+
+    # constructor
+    def __init__(self, num_states, num_actions, model_path):
+
+        # printing some of the values for debugging later
+        print('num_states:', num_states)
+        print('num_actions:', num_actions)
+
+        # defining some data fields
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        # Base model
+        self.model = self.build_model()
+
+        # Target model (essentially a copy of the base model itself)
+        self.model_ = self.build_model()
+
+        # creating model checkpoints and callback functions
+        self.model_checkpoint_1 = model_path + "CarRacing_DDQN_model_best.h5"
+        self.model_checkpoint_2 = model_path + "CarRacing_DDQN_model_per.h5"
+
+        save_best = ModelCheckpoint(
+            self.model_checkpoint_1,
+            monitor='loss',
+            verbose=1,
+            save_best_only=True,
+            mode='min',
+            period=20
+        )
+        save_per = ModelCheckpoint(
+            self.model_checkpoint_2,
+            monitor='loss',
+            verbose=1,
+            save_best_only=False,
+            mode='min',
+            period=400
+        )
+
+        self.callbacks_list = [save_best, save_per]
+
+
+    # CNN that takes the state and outputs the Q values for all of the possible actions
+    # Model
+    #     - in : states
+    #     - out : actions
+    def build_model(self):
+
+        states_in = Input(shape=self.num_states, name='states_in')
+        x = Conv2D(32, (8,8), strides=(4,4), activation='relu', name='conv1')(states_in)
+        x = Conv2D(64, (4,4), strides=(2,2), activation='relu', name='conv2')(x)
+        x = Conv2D(64, (3,3), strides=(1,1), activation='relu', name='conv3')(x)
+        x = Flatten(name='flattened')(x)
+        x = Dense(512, activation='relu')(x)
+        out = Dense(self.num_actions, activation='linear')(x)
+
+        model = Model(states_in, out)
+        self.optimizer = optimizers.Adam(
+            learning_rate=learning_rate,
+            beta_1=0.9,
+            beta_2=0.999,
+            epsilon=None,
+            decay=0.0,
+            amsgrad=False
+        )
+        loss = losses.mse()
+        model.compile(
+            loss=loss,
+            optimizer=self.optimizer
+        )
+        plot_model(
+            model,
+            to_file='model_architecture.png',
+            show_shapes=True
+        )
+
+        return model
+
+
+    # training the function itself
+    def train(self, x, y, epochs=10, verbose=1):
+        self.model.fit(
+            x,
+            y,
+            batch_size=(BATCH_SIZE),
+            epochs=epochs,
+            verbose=verbose,
+            callbacks=self.callbacks_list,
+        )
+
+
+    # predict function
+    def predict(self, state, target=False):
+        if target:
+            # return the Q value given a state from the target network
+            return self.model_.predict(state)
+        else:
+            # return the Q value from the original network
+            return self.model.predict(state)
+
+
+    # predicting only a single state
+    def predict_single_state(self, state, target=False):
+        x = state[np.newaxis, :, :, :]
+        return self.predict(x, target)
+
+
+    # update the target model with the base model weights
+    def target_model_update(self):
+        self.model_.set_weights(self.model.get_weights())
+
